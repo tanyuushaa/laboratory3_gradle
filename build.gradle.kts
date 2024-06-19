2db414b2dd2f81b5d3a1ced15735b8e4b1bbf313
tanyuushaa <rudenko2005w@gmail.com>
2024-06-19 18:11:41 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ version = "1.0-SNAPSHOT"
 
 subprojects {
     apply(plugin = "java")
+
     repositories {
         mavenCentral()
     }
@@ -15,7 +16,55 @@ subprojects {
         testImplementation(platform("org.junit:junit-bom:5.10.0"))
         testImplementation("org.junit.jupiter:junit-jupiter")
     }
+
+    tasks.named<Test>("test") {
+        useJUnitPlatform()
+    }
 }
-tasks.test {
-    useJUnitPlatform()
+
+tasks.register("checkFile") {
+    val filePath = "app2/src/main/resources/text"
+
+    doLast {
+        val file = file(filePath)
+        if (file.exists()) {
+            println("File '$filePath' exists.")
+        } else {
+            println("File '$filePath' does not exist.")
+            throw GradleException("File '$filePath' is missing.")
+        }
+    }
+}
+
+tasks.register("copyFile") {
+    val sourcePath = "app2/src/main/resources/text"
+    val destinationPath = "app2/build/resources/text"
+
+    doLast {
+        val sourceFile = file(sourcePath)
+        val destinationFile = file(destinationPath)
+        if (!destinationFile.parentFile.exists()) {
+            destinationFile.parentFile.mkdirs()
+        }
+        sourceFile.copyTo(destinationFile, overwrite = true)
+        println("File copied to '$destinationPath'.")
+    }
+}
+
+tasks.register("cleanCopyDir") {
+    val directoryPath = "app2/build/resources"
+
+    doLast {
+        val directory = file(directoryPath)
+        if (directory.exists()) {
+            directory.deleteRecursively()
+            println("Directory '$directoryPath' cleaned.")
+        }
+    }
+}
+
+tasks.named("build") {
+    dependsOn("checkFile")
+    dependsOn("cleanCopyDir")
+    dependsOn("copyFile")
 }
